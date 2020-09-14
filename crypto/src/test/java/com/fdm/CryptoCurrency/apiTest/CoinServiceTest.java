5273d222862bba0d7ead3bd001251784f3762fdd
Karen <douk@student.unimelb.edu.au>
2020-09-14 15:44:05 +1000

    test updated

--- crypto/src/test/java/com/fdm/CryptoCurrency/apiTest/CoinServiceTest.java
+++ crypto/src/test/java/com/fdm/CryptoCurrency/apiTest/CoinServiceTest.java
@@ -1,39 +1,69 @@
 package com.fdm.CryptoCurrency.apiTest;
 
-import com.fdm.CryptoCurrency.CryptoCurrencyApplication;
-import com.fdm.CryptoCurrency.model.CryptoCurrency;
-import com.fdm.CryptoCurrency.model.CryptoCurrencyDetail;
+import com.fdm.CryptoCurrency.client.CryptoFeignClient;
+import com.fdm.CryptoCurrency.model.*;
 import com.fdm.CryptoCurrency.service.ClientService;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
-// not using springboot test ,aviod for quicker response
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = CryptoCurrencyApplication.class)
+
 public class CoinServiceTest {
-	
 
-	@Autowired
-	private  ClientService coinService;
+	private static final String AUS_DATE_FORMAT = "dd-MM-yyyy";
+	private static final DateTimeFormatter AUS_DATE_FORMATTER = DateTimeFormatter.ofPattern(AUS_DATE_FORMAT);
 
-	
+	private  ClientService clientService;
+
+	@Mock
+	private CryptoFeignClient cryptoFeignClient;
+
+	@Before
+	public void setup(){
+		initMocks(this);
+		clientService = new ClientService(cryptoFeignClient);
+	}
 
 	@Test
 	public void test_getCurrencyDetail_returns_cd() throws Exception {
-		
-		CryptoCurrencyDetail cd = coinService.getCurrencyDetail("bitcoin");
+		CurrencyPriceDTO currencyPriceDTO = new CurrencyPriceDTO().builder()
+				.aud("12.23")
+				.usd("2.3")
+				.jpy("2342")
+				.build();
+		MarketDataDTO marketDataDTO = new MarketDataDTO().builder().market_cap(currencyPriceDTO).current_price(currencyPriceDTO).price_change_24h_in_currency(currencyPriceDTO).build();
+		CryptoDetailDTO dto = new CryptoDetailDTO().builder()
+				.id("bitcoin")
+				.symbol("bitcoin")
+				.name("bitcoin")
+				.genesis_date("2020-09-14")
+				.last_updated("2020-09-14T01:24:18.219Z")
+				.market_data(marketDataDTO).build();
+
+		HistoryMarketDataDTO historyMarketDataDTO = new HistoryMarketDataDTO().builder().current_price(currencyPriceDTO).build();
+		CryptoHistoryDTO historyDTOdto = new CryptoHistoryDTO().builder().market_data(historyMarketDataDTO).build();
+
+		LocalDate lastWeek = LocalDate.now().minusWeeks(1);
+		String date = AUS_DATE_FORMATTER.format(lastWeek);
+
+		when(cryptoFeignClient.findCurrency("bitcoin")).thenReturn(dto);
+		when(cryptoFeignClient.findHistory("bitcoin",date)).thenReturn(historyDTOdto);
+
+		CryptoCurrencyDetail cd = clientService.getCurrencyDetail("bitcoin");
 
 		assertEquals("bitcoin", cd.getId());
-		assertNotNull(cd.getSymbol());
-		assertNotNull(cd.getName());
+		assertEquals("bitcoin", cd.getSymbol());
+		assertEquals("bitcoin", cd.getName());
 		assertNotNull(cd.getMarket_cap());
 		assertNotNull(cd.getGenesis_date());
 		assertNotNull(cd.getLast_update());
@@ -45,7 +75,18 @@ public class CoinServiceTest {
 
 	@Test
 	public void test_getAll_returns_ccs() throws Exception {
-		ArrayList<CryptoCurrency> cryptoCurrencys = coinService.getAll("aud", "10", "1");
+		StatusUpdate statusUpdate = new StatusUpdate().builder().user_title("abc").created_at("2020-09-14").description("for testing").build();
+		ArrayList<StatusUpdate> statusUpdates = new ArrayList<StatusUpdate>();
+		statusUpdates.add(statusUpdate);
+		StatusUpdateDTO statusUpdateDTO = new StatusUpdateDTO().builder().status_updates(statusUpdates).build();
+		CryptoCurrency cryptoCurrency = new CryptoCurrency().builder().id("bitcoin").current_price("123").market_cap("345677").build();
+		ArrayList<CryptoCurrency> ccs = new ArrayList<CryptoCurrency>();
+		ccs.add(cryptoCurrency);
+		when(cryptoFeignClient.findStatusUpdate("bitcoin")).thenReturn(statusUpdateDTO);
+
+		when(cryptoFeignClient.findMarket("aud","10","1")).thenReturn(ccs);
+
+		ArrayList<CryptoCurrency> cryptoCurrencys = clientService.getAll("aud", "10", "1");
 		CryptoCurrency cc = cryptoCurrencys.get(0);
 		assertEquals("bitcoin", cc.getId());
 		assertNotNull(cc.getCurrent_price());
--- crypto/src/test/java/com/fdm/CryptoCurrency/apiTest/ControllerTest.java
+++ crypto/src/test/java/com/fdm/CryptoCurrency/apiTest/ControllerTest.java
@@ -1,23 +1,25 @@
 package com.fdm.CryptoCurrency.apiTest;
 
 import com.fdm.CryptoCurrency.controllers.CoinRestController;
-import com.fdm.CryptoCurrency.exception.NotFoundCurrencyException;
-import com.fdm.CryptoCurrency.exception.NotFoundPaginationException;
 import com.fdm.CryptoCurrency.model.CryptoCurrency;
 import com.fdm.CryptoCurrency.model.CryptoCurrencyDetail;
 import com.fdm.CryptoCurrency.service.ClientService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ControllerTest {
-	
-	
+
+	private static final String CURRENCY_NOT_FOUND_EXCEPTION = "Currency Not Found!";
+
 	private ClientService coinService;
 	private CryptoCurrencyDetail mockCD;
 	private CryptoCurrency mockCC;
@@ -40,29 +42,19 @@ public class ControllerTest {
 	@Test
 	public void that_getCurrencyDetail_returns_cd() throws Exception {
 		when(coinService.getCurrencyDetail("bitcoin")).thenReturn(mockCD);
-//		CryptoCurrencyDetail cd = coinRestController.getCurrencyDetail("bitcoin");
-//		assertEquals(mockCD,cd);
+		ResponseEntity<?> response = coinRestController.getCurrencyDetail("bitcoin");
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(mockCD, response.getBody());
 	}
-	
+
 	@Test
 	public void that_getAll_returns_all() throws Exception {
 		ArrayList<CryptoCurrency> mockCCs = new ArrayList<CryptoCurrency>();
 		mockCCs.add(mockCC);
 		when(coinService.getAll("usd","10","1")).thenReturn(mockCCs);
-//		ArrayList<CryptoCurrency> cryptoCurrencys = coinRestController.getAll("usd","10","1");
-//		assertEquals(cryptoCurrencys,mockCCs);
-	}
-	
-	@Test(expected = NotFoundCurrencyException.class)
-	public void that_getAll_notFoundCurrency() throws Exception {
-		coinRestController.getAll("abc","10","1");
-		
-	}
-	
-	@Test(expected = NotFoundPaginationException.class)
-	public void that_getAll_notFoundPage() throws Exception {
-		coinRestController.getAll("aud","10","11");
-
+		ResponseEntity<?> response = coinRestController.getAll("usd","10","1");
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(response.getBody(),mockCCs);
 	}
 	
 	
